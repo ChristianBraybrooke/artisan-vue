5d07f0d4668287bb05afccb82a3ae46cc2cd1357
Christian Braybrooke <chris@purplemountmedia.com>
2017-09-11 14:29:38 +0100

    Use Command

--- src/commands/MakeVueComponent.php
+++ src/commands/MakeVueComponent.php
@@ -2,10 +2,12 @@
 
 namespace ChrisBraybrooke\ArtisanVue\Commands;
 
+use Illuminate\Console\Command;
+
 /**
  *
  */
-class MakeVueComponent
+class MakeVueComponent extends Command
 {
 
   function __construct()
